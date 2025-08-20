6cd98f68bee02055b7ac243742d77695178951b3
Fadafranca <fadafranca2@gmail.com>
2025-08-20 15:37:06 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/dio/expcetion/InvestmentNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.dio.expcetion;
+
+public class InvestmentNotFoundException extends RuntimeException {
+
+    public InvestmentNotFoundException(String message) {
+        super(message);
+    }
+}
